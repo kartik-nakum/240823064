678306f541cdd089ef77b79ada56cf63839c44c2
kartik nakum <kartiknakum4559@gmail.com>
2025-07-14 11:24:53 +0530

    1 program

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const surname="NAKUM"
 const name="KARTIK"
 console.log(name)
 console.log(surname)
+
+
+//1
+let num = 7
+if(num % 2 == 0){
+    console.log("number is even")
+}else{
+    console.log("number is odd")
+}
+
