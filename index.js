39c842f50d97f3426ee046e8a34c0be0c5ea8c32
kartik nakum <kartiknakum4559@gmail.com>
2025-07-14 11:41:39 +0530

    2 program

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ if(num % 2 == 0){
     console.log("number is odd")
 }
 
+//2
+const a = 5
+const b = 7
+
+if (a>b){
+    console.log("a is max")
+}else{
+    console.log("b is min")
+}
+
+//3
+const year = 2020
+
+if (year % 4 == 0){
+    console.log(year+"year is leap")
+}else{
+    console.log(`${year}year is no leap`)
+}
